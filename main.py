f85d80f2414e36e9b9fd6be246e04641978b8b2b
chenrohua24 <z7707092003@gmail.com>
2022-07-01 16:34:05 +0800

    Rename config

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ from files import move_by_file_pattern
 parser = ConfigParser()
 parser.read('config.conf')
 
-PHOTOS_FOLDER = parser.get('folder_path', 'photos')
-ORGANIZED_PHOTOS_FOLDER = parser.get('folder_path', 'organized_photos')
+SOURCE_FOLDER = parser.get('folder_path', 'source_folder')
+TARGET_FOLDER = parser.get('folder_path', 'target_folder')
 
 
 def main():
-    move_by_file_pattern(
-        PHOTOS_FOLDER, ORGANIZED_PHOTOS_FOLDER, '*.JPG')
+    move_by_file_pattern(SOURCE_FOLDER, TARGET_FOLDER, '*.JPG')
 
 
 if __name__ == '__main__':
