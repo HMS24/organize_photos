d5dbe8055fce75aec77373ec7ca8a819810a1799
chenrohua24 <z7707092003@gmail.com>
2022-07-01 16:25:53 +0800

    Rename and remove unnecessarily import

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ parser.read('config.conf')
 
 PHOTOS_FOLDER = parser.get('folder_path', 'photos')
 ORGANIZED_PHOTOS_FOLDER = parser.get('folder_path', 'organized_photos')
-ARCHIVE_FOLDER = parser.get('folder_path', 'archive')
 
 
-def move(source, target, file_pattern):
+def move_file_from_source_to_target_by_pattern(source, target, file_pattern):
     source_path = Path(source)
     target_path = Path(target)
     paths = source_path.glob(file_pattern)
@@ -21,7 +20,8 @@ def move(source, target, file_pattern):
 
 
 def main():
-    move(PHOTOS_FOLDER, ORGANIZED_PHOTOS_FOLDER, '*.JPG')
+    move_file_from_source_to_target_by_pattern(
+        PHOTOS_FOLDER, ORGANIZED_PHOTOS_FOLDER, '*.JPG')
 
 
 if __name__ == '__main__':
